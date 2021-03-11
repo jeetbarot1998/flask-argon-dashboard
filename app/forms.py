a3b6e660ae9559dd6eeb10087710d6a105a5ae5f
jeetbarot1998 <jeetbarot1998@gmail.com>
2021-03-11 22:52:41 +0530

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,7 @@ License: MIT
 Copyright (c) 2019 - present AppSeed.us
 """
 
-from flask_wtf          import FlaskForm
+from flask_wtf          import FlaskForm, Form
 from flask_wtf.file     import FileField, FileRequired
 from wtforms            import StringField, TextAreaField, SubmitField, PasswordField
 from wtforms.validators import InputRequired, Email, DataRequired
@@ -17,3 +17,10 @@ class RegisterForm(FlaskForm):
 	username    = StringField  (u'Username'  , validators=[DataRequired()])
 	password    = PasswordField(u'Password'  , validators=[DataRequired()])
 	email       = StringField  (u'Email'     , validators=[DataRequired(), Email()])
+
+class AddProduct(FlaskForm):
+	productname   = StringField ('Productname' , validators=[DataRequired()])
+	specification = StringField ('Specification' , validators=[DataRequired()]) 
+	category =  StringField ('Category' , validators=[DataRequired()]) 
+	price = StringField ('Price' , validators=[DataRequired()])
+
