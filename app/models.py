92e5ac04b4efb12c940d506bcc16899cce93a91b
jeetbarot1998 <jeetbarot1998@gmail.com>
2021-03-11 22:53:11 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -6,6 +6,9 @@ Copyright (c) 2019 - present AppSeed.us
 
 from app         import db
 from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+
 
 class User(UserMixin, db.Model):
 
@@ -24,6 +27,33 @@ class User(UserMixin, db.Model):
 
     def save(self):
 
+        # inject self into db session    
+        db.session.add ( self )
+
+        # commit change and save the object
+        db.session.commit( )
+
+        return self 
+
+class productin(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    productname = db.Column(db.String(75), nullable=False)
+    specification = db.Column(db.String(75), nullable=False)
+    category  = db.Column(db.String(75), nullable=False)
+    price =db.Column(db.String(75), nullable=False)
+    
+    def __init__(self, specification, productname, category, price):
+        self.productname       = productname
+        self.specification   = specification
+        self.category     = category
+        self.price = price 
+    
+    def __repr__(self):
+        # return f'<productin : {self.poductname}'
+        return '<ID >' % (self.id) % (self.productname) % (self.category) % (self.price)
+
+    def saved(self):
+ 
         # inject self into db session    
         db.session.add ( self )
 
